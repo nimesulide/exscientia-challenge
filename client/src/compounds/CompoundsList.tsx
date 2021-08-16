e3c85cd2732d8402f5ccc995e22715ddbfcc9dff
Mate Csonka <mate.csonka92@gmail.com>
2021-08-16 22:19:12 +0200

    WIP - Experimenting with ag-grid

--- client/src/compounds/CompoundsList.tsx
+++ client/src/compounds/CompoundsList.tsx
@@ -1,24 +1,43 @@
 import React from 'react';
-import { List } from '@material-ui/core';
-import { Compound } from './types';
-import CompoundsListItem from './CompoundsListItem';
 import { view } from '@risingstack/react-easy-state';
-import CompoundsStore from './CompoundsStore';
+import { AgGridColumn, AgGridReact } from 'ag-grid-react';
+import { Avatar, makeStyles } from '@material-ui/core';
+import { Compound } from './types';
+
+import 'ag-grid-community/dist/styles/ag-grid.css';
+import 'ag-grid-community/dist/styles/ag-theme-material.css';
 
 interface CompoundListProps {
     compounds: Compound[];
 }
 
-export default view(({ compounds }: CompoundListProps) => (
-    <List>
-        {
-            compounds.map(compound => (
-                <CompoundsListItem
-                    compound={compound}
-                    handleListItemSelect={(compound_id) => CompoundsStore.selectCompound(compound_id)}
-                    selected={compound.compound_id === CompoundsStore.selectedCompoundId}
-                />
-            ))
-        }
-    </List>
-));
+const useStyles = makeStyles((theme) => ({
+    root: {
+        height: 500,
+        width: '100%'
+    }
+}));
+
+export default view(({ compounds }: CompoundListProps) => {
+    const classes = useStyles();
+    const imageCellRenderer = (props: any) => <Avatar variant={'square'} src={`https://exscientia-challenge.s3.eu-central-1.amazonaws.com/${props.value}`} />;
+
+    return (
+        <div className={`ag-theme-material ${classes.root}`}>
+            <AgGridReact
+                rowData={compounds}
+                suppressCellSelection
+                frameworkComponents={{
+                    imageCellRenderer
+                }}
+            >
+                <AgGridColumn field={'image'} cellRenderer={'imageCellRenderer'} autoHeight></AgGridColumn>
+                <AgGridColumn field={'molecular_formula'} sortable></AgGridColumn>
+                <AgGridColumn field={'smiles'}></AgGridColumn>
+                <AgGridColumn field={'molecular_weight'} sortable></AgGridColumn>
+                <AgGridColumn field={'ALogP'} sortable></AgGridColumn>
+                <AgGridColumn field={'num_rings'} sortable></AgGridColumn>
+            </AgGridReact>
+        </div>
+    )
+});
