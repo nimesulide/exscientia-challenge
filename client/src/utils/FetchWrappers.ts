158f8d5c549cd64a546cf562c78471bede3b4087
Mate Csonka <mate.csonka92@gmail.com>
2021-08-16 16:42:38 +0200

    Fetch wrappers for client and route generation handling.

---
+++ client/src/utils/FetchWrappers.ts
@@ -0,0 +1,56 @@
+const convertResponseToJson = (response: Response) => response.json();
+
+const createRequest = (method: string, data: any): RequestInit => ({
+    method: method,
+    credentials: 'same-origin',
+    headers: {
+        'content-type': 'application/json'
+    },
+    body: JSON.stringify(data)
+});
+
+const post = async (url: RequestInfo, data: any) => {
+    try {
+        return await fetch(url, createRequest('POST', data)).then(convertResponseToJson);
+    } catch (error) {
+        console.log(error);
+    }
+};
+
+const put = async (url: RequestInfo, data: any) => {
+    try {
+        return await fetch(url, createRequest('PUT', data)).then(convertResponseToJson);
+    } catch (error) {
+        console.log(error);
+    }
+};
+
+const patch = async (url: RequestInfo, data: any) => {
+    try {
+        const response = await fetch(url, createRequest('PATCH', data));
+        return await convertResponseToJson(response);
+    } catch (error) {
+        console.log(error);
+    }
+};
+
+const remove = async (url: RequestInfo, data: any) => {
+    try {
+        const response = await fetch(url, createRequest('DELETE', data));
+        return await convertResponseToJson(response);
+    } catch (error) {
+        console.log(error);
+    }
+};
+
+
+const get = async (url: RequestInfo) => {
+    try {
+        return await fetch(url, { credentials: 'same-origin' }).then(convertResponseToJson);
+    }
+    catch (error) {
+        console.log(error);
+    }
+};
+
+export { post, get, put, patch, remove };
---
+++ client/src/utils/Routes.ts
@@ -0,0 +1,12 @@
+import { compile } from "path-to-regexp";
+
+export enum Routes {
+    GET_COMPOUND_LIST = '/api/compounds/',
+    GET_COMPOUND_DETAILS = '/api/compounds/:compound_id'
+};
+
+export const generatePath = (route: Routes, params: { [key: string]: string | number }) => {
+    const toPath = compile(route, {encode: encodeURIComponent});
+
+    return toPath(params);
+};
